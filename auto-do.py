daa9f132b072c10d84691b55431e9dd0e9d3ef1d
F-T-Otaku <53745554+F-T-Otaku@users.noreply.github.com>
2021-02-01 01:29:35 +0800

    Update auto-do.py

--- auto-do.py
+++ auto-do.py
@@ -6,7 +6,6 @@ from bs4 import BeautifulSoup
 
 rsa_e = "010001"
 rsa_m = "008aed7e057fe8f14c73550b0e6467b023616ddc8fa91846d2613cdb7f7621e3cada4cd5d812d627af6b87727ade4e26d26208b7326815941492b2204c3167ab2d53df1e3a2c9153bdb7c8c2e968df97a5e7e01cc410f92c4c2c2fba529b3ee988ebc1fca99ff5119e036d732c368acf8beba01aa2fdafa45b21e4de4928d0d403"
-output = "<br>"
 
 def log(s: str):
     timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
@@ -161,6 +160,7 @@ def doReport(person):
 
 if __name__ == '__main__':
     import sys
+    output = "<br>"
     person = {
         "CASUsername": sys.argv[1],
         "CASPassword": sys.argv[2],
