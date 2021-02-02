c4b24f670ad4c1a2ea218953a6f50bb86ac1720e
F-T-Otaku <53745554+F-T-Otaku@users.noreply.github.com>
2021-02-02 18:06:28 +0800

    Fix bug

--- auto-do.py
+++ auto-do.py
@@ -10,13 +10,13 @@ rsa_m = "008aed7e057fe8f14c73550b0e6467b023616ddc8fa91846d2613cdb7f7621e3cada4cd
 def log(s: str):
     global output
     timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
-    print(f"[{timestamp}]\t{s}\n")
+    print(f"[{timestamp}]\t{s}")
     output = output + "[" + timestamp + "]" + s + "  " + "\n\n"
 
 
 def genRSAPasswd(passwd, e, m):
-    #别问我为啥rsa加密要这么写，傻逼cas
-    #参考https://www.cnblogs.com/himax/p/python_rsa_no_padding.html
+    # 别问我为啥rsa加密要这么写，傻逼cas
+    # 参考https://www.cnblogs.com/himax/p/python_rsa_no_padding.html
     m = int.from_bytes(bytearray.fromhex(m), byteorder='big')
     e = int.from_bytes(bytearray.fromhex(e), byteorder='big')
     plaintext = passwd[::-1].encode("utf-8")
@@ -48,6 +48,22 @@ def DoSUESCasLogin(username, password, sess):
         return False
 
 
+def lower_json(json_info):
+    if isinstance(json_info, dict):
+        for key in list(json_info.keys()):
+            if key.islower():
+                lower_json(json_info[key])
+            else:
+                key_lower = key.lower()
+                json_info[key_lower] = json_info[key]
+                del json_info[key]
+                lower_json(json_info[key_lower])
+
+    elif isinstance(json_info, list):
+        for item in json_info:
+            lower_json(item)
+
+
 def doReport(person):
     username = person["CASUsername"]
     password = person["CASPassword"]
@@ -56,27 +72,26 @@ def doReport(person):
     sess.keep_alive = False
     sess.headers.update({
         "Accept":
-        "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
         "Accept-Encoding":
-        "gzip, deflate",
+            "gzip, deflate",
         "Accept-Language":
-        "zh-CN,zh;q=0.9",
+            "zh-CN,zh;q=0.9",
         "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36 Edg/87.0.664.75"
+            "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SE 2.X MetaSr 1.0; SE 2.X MetaSr 1.0; .NET CLR 2.0.50727; SE 2.X MetaSr 1.0)"
     })
     res = DoSUESCasLogin(username, password, sess)
     if not res:
         return False, "LOGIN FAIL"
     url = "https://cas.sues.edu.cn/cas/login?service=https%3A%2F%2Fworkflow.sues.edu.cn%2Fdefault%2Fwork%2Fshgcd%2Fjkxxcj%2Fjkxxcj.jsp"
-    res = sess.get(url)  #cas 跳转登录
+    res = sess.get(url)  # cas 跳转登录
     newHeader = {
         "Accept": "*/*",
         "Accept-Encoding": "gzip, deflate, br",
         "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
         "Content-Type": "text/json",
         "Origin": "https://workflow.sues.edu.cn",
-        "Referer":
-        "https://workflow.sues.edu.cn/default/work/shgcd/jkxxcj/jkxxcj.jsp",
+        "Referer":"https://workflow.sues.edu.cn/default/work/shgcd/jkxxcj/jkxxcj.jsp",
         "Sec-Fetch-Dest": "empty",
         "Sec-Fetch-Mode": "cors",
         "Sec-Fetch-Site": "same-origin",
@@ -94,69 +109,73 @@ def doReport(person):
     now = time_peking.strftime("%Y-%m-%d %H:%M")
 
     # 在这里你可以填写过去或者未来的日期(
-    # timeType = "上午"
-    # now = "2021-01-31 8:11"
+    # timeType = "下午"
+    # now = "2021-02-02 17:51"
 
-    log("Time Peking: " + now)
+    log("Time Peking: " + now + " " + timeType)
+    
+    # 1
     requestJsonFirst = {
         "params": {
             "empcode": username
         },
         "querySqlId": "com.sudytech.work.shgcd.jkxxcj.jkxxcj.queryEmp"
     }
-    requestJsonSecond = {
+    sess.post(
+        "https://workflow.sues.edu.cn/default/work/shgcd/jkxxcj/com.sudytech.portalone.base.db.queryBySqlWithoutPagecond.biz.ext",
+        json=requestJsonFirst)
+
+    # 获取上一次的数据
+    requestLastJson = {
         "params": {
             "empcode": username
         },
         "querySqlId": "com.sudytech.work.shgcd.jkxxcj.jkxxcj.queryNear"
     }
-    sess.post(
-        "https://workflow.sues.edu.cn/default/work/shgcd/jkxxcj/com.sudytech.portalone.base.db.queryBySqlWithoutPagecond.biz.ext",
-        json=requestJsonFirst)
-    resSecond = sess.post(
+    resLast = sess.post(
         "https://workflow.sues.edu.cn/default/work/shgcd/jkxxcj/com.sudytech.portalone.base.db.queryBySqlWithoutPagecond.biz.ext",
         json=requestJsonSecond)
-    if len(resSecond.json()["list"]) == 0:
+    
+    if len(resLast.json()["list"]) == 0:
         return False, "GET LAST REPORT FAIL"
-    person = resSecond.json()["list"][0]
+    person = resLast.json()["list"][0]
+    lower_json(person)
+    # 上报
     updateData = {
         "params": {
-            "id": person["ID"],
-            "sqrid": person["SQRID"],
-            "sqbmid": person["SQBMID"],
-            "rysf": person["RYSF"],
-            "sqrmc": person["SQRMC"],
-            "gh": person["GH"],
-            "sfzh": person["SFZH"],
-            "sqbmmc": person["SQBMMC"],
-            "xb": person["XB"],
-            "lxdh": person["LXDH"],
-            "nl": person["NL"],
+            "sqrid": person["sqrid"],
+            "sqbmid": person["sqbmid"],
+            "rysf": person["rysf"],
+            "sqrmc": person["sqrmc"],
+            "gh": person["gh"],
+            "sfzh": person["sfzh"],
+            "sqbmmc": person["sqbmmc"],
+            "xb": person["xb"],
+            "lxdh": person["lxdh"],
+            "nl": person["nl"],
             "tjsj": now,
-            "xrywz": person["XRYWZ"],
-            "sheng": person["SHENG"],
-            "shi": person["SHI"],
-            "qu": person["QU"],
-            "jtdzinput": person["JTDZINPUT"],
-            "gj": "",
-            "jtgj": "",
-            "jkzk": person["JKZK"],
-            "jkqk": person["JKQK"],
+            "xrywz": person["xrywz"],
+            "sheng": person["sheng"],
+            "shi": person["shi"],
+            "qu": person["qu"],
+            "jtdzinput": person["jtdzinput"],
+            "gj": person["gj"],
+            "jtgj": person["jtgj"],
+            "jkzk": person["jkzk"],
+            "jkqk": person["jkqk"],
             "tw": str(round(random.uniform(36.1, 36.9), 1)),
             "sd": timeType,
-            "bz": "",
+            "bz": person["bz"],
             "_ext": "{}"
         }
     }
-    log(updateData["params"]["gh"] + "\t" + "gentemp:" +
-        updateData["params"]["tw"])
+    log(updateData["params"]["gh"] + "\t" + "gentemp:" + updateData["params"]["tw"])
     url = "https://workflow.sues.edu.cn/default/work/shgcd/jkxxcj/com.sudytech.work.shgcd.jkxxcj.jkxxcj.saveOrUpdate.biz.ext"
     finalRes = sess.post(url, json=updateData)
     if finalRes.json()['result']["success"]:
         return True, None
     else:
-        return False, "[" + finalRes.json(
-        )['result']['errorcode'] + "]" + finalRes.json()['result']['msg']
+        return False, "[" + finalRes.json()['result']['errorcode'] + "]" + finalRes.json()['result']['msg']
 
 
 if __name__ == '__main__':
@@ -177,13 +196,13 @@ if __name__ == '__main__':
     sess.keep_alive = False
     sess.headers.update({
         "Accept":
-        "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
         "Accept-Encoding":
-        "gzip, deflate",
+            "gzip, deflate",
         "Accept-Language":
-        "zh-CN,zh;q=0.9",
+            "zh-CN,zh;q=0.9",
         "User-Agent":
-        "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SE 2.X MetaSr 1.0; SE 2.X MetaSr 1.0; .NET CLR 2.0.50727; SE 2.X MetaSr 1.0)"
+            "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SE 2.X MetaSr 1.0; SE 2.X MetaSr 1.0; .NET CLR 2.0.50727; SE 2.X MetaSr 1.0)"
     })
     res = DoSUESCasLogin(person["CASUsername"], person["CASPassword"], sess)
     if res:
